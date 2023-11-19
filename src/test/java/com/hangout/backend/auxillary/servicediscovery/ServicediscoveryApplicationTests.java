2f84c8780a01651635e642ee23275a87325d3eb6
Soumalya Bhattacharya <soumalyabhattacharya6@gmail.com>
2023-11-19 13:25:08 +0530

    Initializing Service Discovery With Eureka Server

---
+++ src/test/java/com/hangout/backend/auxillary/servicediscovery/ServicediscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hangout.backend.auxillary.servicediscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicediscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
