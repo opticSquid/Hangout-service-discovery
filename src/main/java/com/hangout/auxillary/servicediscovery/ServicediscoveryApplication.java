511aa4ae69c6a6a6d017099889b6aabdabe22335
Soumalya Bhattacharya <soumalyabhattacharya6@gmail.com>
2023-12-24 11:41:06 +0530

    removing backend package in the push to conforming to a uniform package system

--- src/main/java/com/hangout/auxillary/servicediscovery/ServicediscoveryApplication.java
+++ src/main/java/com/hangout/auxillary/servicediscovery/ServicediscoveryApplication.java
@@ -1,4 +1,4 @@
-package com.hangout.backend.auxillary.servicediscovery;
+package com.hangout.auxilliary.servicediscovery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
